5761acd9da4d864a33d958081f2388c4f4159df9
{haptism} <haptism007@gmail.com>
2019-03-28 12:17:06 +0900

    Initial commit

---
+++ src/com/iu/member/Member.java
@@ -0,0 +1,52 @@
+package com.iu.member;
+
+// 멤버에 데이터를 저장하기 위한 클래스
+
+public class Member {
+	private String id;
+	private String pw;
+	private int age;
+	private String email;
+	private String phone;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
